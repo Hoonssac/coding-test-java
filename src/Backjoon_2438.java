87d503c6961f75100f01b41301d90397f53a698b
b1uesoda <b1uesoda@naver.com>
2024-07-10 17:13:34 +0900

    Backjoon_2438

---
+++ src/Backjoon_2438.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Backjoon_2438 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		for (int i = 0; i < count; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
