01cd1009a318370b8f1d93526de56e6a338c5a2b
b1uesoda <b1uesoda@naver.com>
2024-09-02 21:53:10 +0900

    Baekjoon_1260

--- src/Baekjoon_1260.java
+++ src/Baekjoon_1260.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 
 public class Baekjoon_1260 {
@@ -61,50 +60,5 @@ public class Baekjoon_1260 {
         bfs(start);
 
         sc.close();
-=======
-import java.util.Scanner;
-
-public class Baekjoon_1260 {
-	static int[][] graph;
-	static boolean[] visited;
-	static int size;
-	static int start;
-	
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String[] setting = sc.nextLine().split(" ");
-		size = Integer.parseInt(setting[0]);
-		start = Integer.parseInt(setting[2]);
-		graph = new int[size + 1][size + 1];
-		visited = new boolean[size + 1];
-		
-		// 그래프 입력
-		for (int i = 0; i < Integer.parseInt(setting[1]); i++) {
-			String[] input = sc.nextLine().split(" ");
-			int a = Integer.parseInt(input[0]);
-			int b = Integer.parseInt(input[1]);
-			graph[a][b] = 1;
-			graph[b][a] = 1;
-		}
-		
-		dfs(start);
-		sc.close();
-	}
-	
-	static void dfs(int node) {
-		visited[node] = true;
-		System.out.print(node + " ");
-		
-		for (int i = 1; i <= size; i++) {
-			if (!visited[i] && graph[node][i] == 1) {
-				dfs(i);
-			}
-		}
-	}
-
-    static void bfs(int node) {
->>>>>>> 7c8416ac8b417fed1a12a103e7fd0ab686b56eaa
-        
     }
 }
