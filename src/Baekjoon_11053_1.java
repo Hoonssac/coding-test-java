a2d3dd095ab9b5d553b7da9f96a5c4c2a57d1cd5
b1uesoda <b1uesoda@naver.com>
2024-11-18 23:34:53 +0900

    Baekjoon_11053

---
+++ src/Baekjoon_11053_1.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baekjoon_11053_1 {
+    static int [] arr;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        String[] s = br.readLine().split(" ");
+
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
+        int [] dp = new int[n];
+        dp[0]=1;
+        for(int i=1; i<n; i++){
+            dp[i]=1;
+            for(int j=0; j<i; j++){
+                if(arr[i]>arr[j] && dp[j]>=dp[i]){
+                    dp[i]=dp[j]+1;
+                }
+            }
+        }
+        Arrays.sort(dp);
+        System.out.println(dp[n-1]);
+    }
+}
